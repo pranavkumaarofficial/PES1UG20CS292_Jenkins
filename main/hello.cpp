031317b3278a14887ea052ec0e2d4e25eb2214ce
Pranav Kumaar <79296445+pranavkumaarofficial@users.noreply.github.com>
2023-02-17 22:41:00 +0530

    Add files via upload

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World PES1UG20CS064\n";
+  cout << "Hello, Jenkins PES1UG20CS064\n";
+  cout << "I have successfully built and run PES1UG20CS064\n";
+  return 0;
+}
